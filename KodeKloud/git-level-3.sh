610c9cf764972110639d1ed4fb1be2dbdc733724
yuvaraj-rk <yuvarajrk2007@gmail.com>
2026-05-22 23:48:50 +0200

    Kodekloud Git Level-3 day 2 challenges completed

--- KodeKloud/git-level-3.sh
+++ KodeKloud/git-level-3.sh
@@ -31,7 +31,31 @@ cat info.txt
 
 #Day-2: 
 #--------------------------
-sshpass -p "Bl@kW" ssh -o StrictHostKeyChecking=no natasha@ststor01
+sshpass -p "Max_pass123" ssh -o StrictHostKeyChecking=no max@ststor01
+pwd
+ls
+cd story-blog/
+git status
+git branch
+git log
+git switch master
+git log
+git log --oneline
+git switch story/fox-and-grapes
+git log --oneline
+#perform PR and merge PR to master branch in Gitea UI webpage
+git switch master
+git status
+#Check content befor pull from remote repo
+ls
+git log --oneline
+#Pull merged changes from remote repot
+git pull origin master
+#Confirm the merge status via log history
+git log --oneline
+#verify merged files present in master branch current directory
+ls
+
 
 #Day-3: 
 #--------------------------
