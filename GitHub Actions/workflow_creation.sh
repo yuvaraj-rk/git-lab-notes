c6c6482f09323a7818d4d1400b5e44116f825dd3
yuvaraj-rk <yuvarajrk2007@gmail.com>
2026-05-23 20:51:27 +0200

    GitHub Actions hands-on Simple Commands as part of Workflow creation practiced

--- GitHub Actions/workflow_creation.sh
+++ GitHub Actions/workflow_creation.sh
@@ -78,3 +78,45 @@ jobs:
         run: echo "Hello, World!"
 labex:github-actions-demo/ (main) $ 
 ###BLOCK-COMMENT
+
+
+######### GitHub Actions Running Simple Commands #########
+
+labex:workflows/ (main*) $ mv simple-commands.yml\# simple-commands.yml   
+labex:workflows/ (main*) $ ls                                          
+hello-world.yml  main.yml  simple-commands.yml
+labex:workflows/ (main*) $ git add .github/workflows/simple-commands.yml
+warning: could not open directory '.github/workflows/.github/workflows/': No such file or directory
+fatal: pathspec '.github/workflows/simple-commands.yml' did not match any files
+labex:workflows/ (main*) $ cd ..
+labex:.github/ (main*) $ cd ..
+labex:github-actions-demo/ (main*) $ git add .github/workflows/simple-commands.yml
+labex:github-actions-demo/ (main*) $ git commit -m "Add simple commands workflow"
+[main d4dd7d7] Add simple commands workflow
+ 1 file changed, 15 insertions(+)
+ create mode 100644 .github/workflows/simple-commands.yml
+labex:github-actions-demo/ (main) $ git push
+Enumerating objects: 8, done.
+Counting objects: 100% (8/8), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (5/5), 605 bytes | 605.00 KiB/s, done.
+Total 5 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/yuvaraj-rk/github-actions-demo.git
+   f5846d5..d4dd7d7  main -> main
+labex:github-actions-demo/ (main) $ cat .github/workflows/simple-commands.yml
+name: Simple Commands
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Echo Hello
+        run: echo "Hello, World!"
+
+      - name: Show Date
+        run: date
+
+      - name: List Files
+        run: ls -la
